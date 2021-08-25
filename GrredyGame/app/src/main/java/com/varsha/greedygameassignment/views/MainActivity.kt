54d2ef68cd208d35874748399059e62918aab383
masai <varshakumari8999@gmail.com>
2021-08-25 10:44:40 +0530

    Dependency SetUP

--- GrredyGame/app/src/main/java/com/varsha/greedygameassignment/views/MainActivity.kt
+++ GrredyGame/app/src/main/java/com/varsha/greedygameassignment/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.varsha.greedygameassignment
+package com.varsha.greedygameassignment.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.varsha.greedygameassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
